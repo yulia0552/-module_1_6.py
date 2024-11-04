8901e2491e5d8a881dde9919e78a2e981417e718
Юлия <veberulia530@gmail.com>
2024-11-04 14:37:25 +0300

    Add files via upload

---
+++ modyle_1_6.py
@@ -0,0 +1,18 @@
+date_book= {"Oleg": 1978, "Dima" : 1973,"Olga": 1981}
+print (date_book)
+print(date_book.get ("Oleg"))
+print(date_book.get ("Masha"))
+date_book. update({"Elena": 1985 ,"Fedor": 1975 })
+print (date_book)
+del date_book ["Dima"]
+print (date_book )
+list_=[3,4,5,4, "зима", 14.04 ,5,5 ]
+list_= set(list_)
+print (list_)
+print (list_.add (35))
+print (list_.add ("лето"))
+print (list_. discard ("зима"))
+print (list_)
+
+
+
